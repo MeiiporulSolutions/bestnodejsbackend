24be424a0a7bcd5989bf5ff5f6eca555e2d62988
seethalakshmi <meiiporulsolutions17@gmail.com>
2024-03-20 12:54:48 +0530

    transfervalidator

--- transfer/transferRoutes.js
+++ transfer/transferRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { transferPatient,transferGet } = require('./transferController');
-
+const { transferPatientValidationRules } = require('./transferValidator');
 //transfer router:
-router.post('/tpsss',transferPatient);
+router.post('/tpsss', transferPatientValidationRules(),transferPatient);
 //transfer Get:
 router.get('/transferGet', transferGet)
 module.exports = router;
